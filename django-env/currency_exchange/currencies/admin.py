bb57b063f4545ec85fbe439a7cf5e7523b71459d
Piotr <63730402+elokhar@users.noreply.github.com>
2024-11-22 13:52:22 +0100

    Add admin interface

--- django-env/currency_exchange/currencies/admin.py
+++ django-env/currency_exchange/currencies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ExchangeRateAdmin(admin.ModelAdmin):
+  list_display = ("currency_pair", "exchange_rate",)
+
+admin.site.register(Currency)
+admin.site.register(ExchangeRate, ExchangeRateAdmin)
--- django-env/currency_exchange/currencies/models.py
+++ django-env/currency_exchange/currencies/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Currency(models.Model):
     code = models.CharField(max_length=3)
     reverse_rate = models.BooleanField(default=False)
+    def __str__(self):
+        return f"{self.code}"
 
 class ExchangeRate(models.Model):
     currency_pair = models.CharField(max_length=6)

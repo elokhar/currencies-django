22ad29a374a91be7e9f4cf7be48c03d648e0c629
Piotr <63730402+elokhar@users.noreply.github.com>
2024-11-24 00:45:27 +0100

    Improve exchange rate endpoint and add more tests
    - add calculating exchange rates between currencies in database other
      than USD (e.g. PLN to EUR exchange rate)
    - add changing exhchange rate direction to the one provided in the request if need (e.g. if user asks for JPY/USD they will get JPY to USD, even if the database has only USD/JPY)
    - add more endpoint tests cases, both for new and existing functionality

--- django-env/currency_exchange/currencies/tests.py
+++ django-env/currency_exchange/currencies/tests.py
@@ -24,26 +24,90 @@ class CurrrencyAPITests(APITestCase, URLPatternsTestCase):
         response = self.client.delete(url, format='json')
         self.assertTrue(status.is_client_error(response.status_code))
 
-    def test_can_get_exchange_rate(self):
-        test_rate = ExchangeRate(currency_pair='ABCXYZ', exchange_rate=1.123)
+    def test_can_get_exchange_rate_to_USD(self):
+        test_rate = ExchangeRate(currency_pair='ABCUSD', exchange_rate=1.123)
         test_rate.save()
+        url=('/currency/ABC/USD/')
+        response = self.client.get(url, format='json')
+        self.assertTrue(status.is_success(response.status_code))
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data, [{'currency_pair':'ABCUSD', 'exchange_rate':1.123}])
+
+    def test_can_get_exchange_rate_from_USD(self):
+        test_rate = ExchangeRate(currency_pair='USDABC', exchange_rate=1.123)
+        test_rate.save()
+        url=('/currency/USD/ABC/')
+        response = self.client.get(url, format='json')
+        self.assertTrue(status.is_success(response.status_code))
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data, [{'currency_pair':'USDABC', 'exchange_rate':1.123}])
+
+    def test_can_get_reversed_exchange_rate_to_USD(self):
+        test_rate = ExchangeRate(currency_pair='USDABC', exchange_rate=2)
+        test_rate.save()
+        url=('/currency/ABC/USD/')
+        response = self.client.get(url, format='json')
+        self.assertTrue(status.is_success(response.status_code))
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data, [{'currency_pair':'ABCUSD', 'exchange_rate':1/2}])
+
+    def test_can_get_reversed_exchange_rate_from_USD(self):
+        test_rate = ExchangeRate(currency_pair='ABCUSD', exchange_rate=2)
+        test_rate.save()
+        url=('/currency/USD/ABC/')
+        response = self.client.get(url, format='json')
+        self.assertTrue(status.is_success(response.status_code))
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data, [{'currency_pair':'USDABC', 'exchange_rate':1/2}])
+
+    def test_can_get_exchange_rate_not_to_USD(self):
+        test_rate1 = ExchangeRate(currency_pair='ABCUSD', exchange_rate=2)
+        test_rate1.save()
+        test_rate2 = ExchangeRate(currency_pair='DEFUSD', exchange_rate=8)
+        test_rate2.save()
+        url=('/currency/ABC/DEF/')
+        response = self.client.get(url, format='json')
+        self.assertTrue(status.is_success(response.status_code))
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data, [{'currency_pair':'ABCDEF', 'exchange_rate':1/4}])
+
+    def test_can_get_exchange_rate_not_to_USD_one_reversed(self):
+        test_rate1 = ExchangeRate(currency_pair='USDABC', exchange_rate=1/2)
+        test_rate1.save()
+        test_rate2 = ExchangeRate(currency_pair='DEFUSD', exchange_rate=8)
+        test_rate2.save()
+        url=('/currency/ABC/DEF/')
+        response = self.client.get(url, format='json')
+        self.assertTrue(status.is_success(response.status_code))
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data, [{'currency_pair':'ABCDEF', 'exchange_rate':1/4}])
+
+    def test_can_get_exchange_rate_not_to_USD_both_reversed(self):
+        test_rate1 = ExchangeRate(currency_pair='USDABC', exchange_rate=1/2)
+        test_rate1.save()
+        test_rate2 = ExchangeRate(currency_pair='USDDEF', exchange_rate=1/8)
+        test_rate2.save()
+        url=('/currency/ABC/DEF/')
+        response = self.client.get(url, format='json')
+        self.assertTrue(status.is_success(response.status_code))
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data, [{'currency_pair':'ABCDEF', 'exchange_rate':1/4}])
+
+    def test_request_both_currencies_not_in_database(self):
         url=('/currency/ABC/XYZ/')
         response = self.client.get(url, format='json')
-        self.assertTrue(status.is_success(response.status_code))
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data, [{'currency_pair':'ABCXYZ', 'exchange_rate':1.123}])
+        self.assertTrue(response.status_code==status.HTTP_400_BAD_REQUEST)
 
-    def test_can_get_reversed_exchange_rate(self):
-        test_rate = ExchangeRate(currency_pair='ABCXYZ', exchange_rate=1.123)
-        test_rate.save()
-        url=('/currency/XYZ/ABC/')
+    def test_request_one_currency_not_in_database(self):
+        test_rate1 = ExchangeRate(currency_pair='USDABC', exchange_rate=1/2)
+        test_rate1.save()
+        url=('/currency/ABC/XYZ/')
         response = self.client.get(url, format='json')
-        self.assertTrue(status.is_success(response.status_code))
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data, [{'currency_pair':'ABCXYZ', 'exchange_rate':1.123}])
+        self.assertTrue(response.status_code==status.HTTP_400_BAD_REQUEST)
 
 
     def test_cant_delete_exchange_rate(self):
         url = ('/currency/ABC/XYZ/')
         response = self.client.delete(url, format='json')
         self.assertTrue(status.is_client_error(response.status_code))
+        
--- django-env/currency_exchange/currencies/views.py
+++ django-env/currency_exchange/currencies/views.py
@@ -10,6 +10,18 @@ from currencies.serializers import *
 def welcome(request):
     return HttpResponse("Welcome to the exchange rate information app!")
 
+def get_rate_to_USD(curr):
+    rate = ExchangeRate.objects.all().filter(currency_pair=curr+'USD')
+    if rate:
+        return rate[0].exchange_rate
+    else:
+        rate = ExchangeRate.objects.all().filter(currency_pair='USD'+curr)
+        if rate:
+            return 1.0/(rate[0].exchange_rate)
+        else:
+            # print(f"currency_{curr}_not_in_database")
+            return None 
+
 @api_view(['GET'])
 def currency_list(request):
     currencies = Currency.objects.all().order_by('code')
@@ -18,9 +30,27 @@ def currency_list(request):
 
 @api_view(['GET'])
 def rate_list(request, curr1, curr2):
-    rate = ExchangeRate.objects.all().filter(currency_pair=curr1+curr2)
-    if not rate:
-        rate = ExchangeRate.objects.all().filter(currency_pair=curr2+curr1)
-
-    serializer = ExchangeRateSerializer(rate, many=True)
-    return Response(serializer.data)
+    if curr1 == 'USD' or curr2 == 'USD':
+        rate_query_result = ExchangeRate.objects.all().filter(currency_pair=curr1+curr2)
+        if not rate_query_result:
+            rate_query_result = ExchangeRate.objects.all().filter(currency_pair=curr2+curr1)
+        if rate_query_result:
+            rate_obj = rate_query_result[0]
+            if rate_obj.currency_pair != curr1 + curr2:
+                rate_obj.currency_pair = curr1 + curr2
+                rate_obj.exchange_rate = 1.0/rate_obj.exchange_rate
+        else:
+            rate_obj = None
+    else:
+        curr1_to_USD = get_rate_to_USD(curr1)
+        curr2_to_USD = get_rate_to_USD(curr2)
+        if curr1_to_USD and curr2_to_USD:
+            curr1_to_curr2 = curr1_to_USD / curr2_to_USD
+            rate_obj = ExchangeRate(currency_pair=curr1+curr2, exchange_rate=curr1_to_curr2)
+        else:
+            rate_obj = None
+    if rate_obj:
+        serializer = ExchangeRateSerializer([rate_obj], many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
